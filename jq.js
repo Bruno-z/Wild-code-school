511785fe60dcae2abd76354c96f7cb7cbc7fd734
Bruno-z <bruno.zilio31@gmail.com>
2017-01-15 22:35:03 +0100

    premier commit

---
+++ jq.js
@@ -0,0 +1,22 @@
+// Image swap
+
+$('#elsa').delay(1000).fadeOut(2500, function(){
+	$(this).attr('src','images/background04.jpg').fadeIn(1000);
+});
+
+// Event hover
+$('h2').hover(function(){
+	$(this).css('color', 'red');
+});
+
+//
+$('p').on( 'click', function () {
+	$(this).toggleClass('steelblue');
+    alert("Bonjour Wild code school!")
+});
+
+$('#elsa').on('mouseover', function() {
+	$('#citation1').css('opacity', '1')
+	$('#citation1').css('transition', '2s linear')
+});
+
